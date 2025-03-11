368888e1abb0e412590fbdb9b8e53d556e12b544
Toby <tobyeclark@gmail.com>
2025-03-11 16:22:53 +0000

    Added function to convert a list into a cloze list

--- gui.py
+++ gui.py
@@ -3,9 +3,27 @@ from aqt.qt import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout
 from aqt import mw
 
 
+def convert_to_list(data: list[str]) -> list[str]:
+    length = len(data)
+    data_list: list[str] = [f"1. {{{{c1::{data[0]}}}}}\n" + "".join([f"{j + 2}.\n" for j in range(length - 1)])]
+    for i in range(length):
+        if i == 0:
+            continue
+        card = ""
+        for j in range(length):
+            if j == i-1:
+                card += f"{j + 1}. {data[i-1]}\n"
+            elif j == i:
+                card += f"{j + 1}. {{{{c1::{data[i]}}}}}\n"
+            else:
+                card += f"{j + 1}.\n"
+        data_list.append(card)
+    return data_list
+
+
 class ListInput(QDialog):
     def __init__(self, editor: Editor):
-        super().__init__()
+        super().__init__(mw)
 
         self.editor = editor
         self.setWindowTitle("Values to List")
@@ -40,7 +58,8 @@ class ListInput(QDialog):
 
     def ok_clicked(self):
         data = self.accumulate_data()
-        print(data)
+        cards = convert_to_list(data)
+        print(cards)
 
     def add_cloze(self, data: str) -> None:
         model = mw.col.models.by_name("_Cloze")
@@ -48,3 +67,8 @@ class ListInput(QDialog):
         new_note["Text"] = data
         deck = mw.col.decks.id_for_name("Test")
         mw.col.add_note(new_note, deck)
+
+
+if __name__ == '__main__':
+    myList = convert_to_list(["One", "Two", "Three", "Four"])
+    [print(item) for item in myList]
