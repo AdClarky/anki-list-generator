bc8eafd2e7be72965c61ab1f90d7f86a9afdc364
Toby <tobyeclark@gmail.com>
2025-03-11 15:37:46 +0000

    Changed labels and fields to be added via a loop, added an accumulate data function to gather it al

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from aqt.editor import Editor
 from aqt.qt import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from aqt import mw
 
 
 class ListInput(QDialog):
@@ -10,33 +11,40 @@ class ListInput(QDialog):
         self.setWindowTitle("Values to List")
         self.setGeometry(100, 100, 300, 200)
 
-        # Labels and Input Fields
-        self.label1 = QLabel("Enter first string:")
-        self.input1 = QLineEdit(self)
+        self.inputLabels: list[QLabel] = []
+        self.inputFields: list[QLineEdit] = []
 
-        self.label2 = QLabel("Enter second string:")
-        self.input2 = QLineEdit(self)
+        for i in range(3):
+            self.inputLabels.append(QLabel(f"String {i + 1}:"))
+            self.inputFields.append(QLineEdit(self))
 
-        self.label3 = QLabel("Enter third string:")
-        self.input3 = QLineEdit(self)
-
-        # Buttons
         self.ok_button = QPushButton("OK", self)
         self.cancel_button = QPushButton("Cancel", self)
 
-        # Layout
         layout = QVBoxLayout()
-        layout.addWidget(self.label1)
-        layout.addWidget(self.input1)
-        layout.addWidget(self.label2)
-        layout.addWidget(self.input2)
-        layout.addWidget(self.label3)
-        layout.addWidget(self.input3)
+        for label, field in zip(self.inputLabels, self.inputFields):
+            layout.addWidget(label)
+            layout.addWidget(field)
+
         layout.addWidget(self.ok_button)
         layout.addWidget(self.cancel_button)
 
         self.setLayout(layout)
 
-        # Button Connections
-        self.ok_button.clicked.connect(self.accept)
+        self.ok_button.clicked.connect(self.ok_clicked)
         self.cancel_button.clicked.connect(self.reject)
+
+    def accumulate_data(self) -> list[str]:
+        data = [field.text() for field in self.inputFields]
+        return data
+
+    def ok_clicked(self):
+        data = self.accumulate_data()
+        print(data)
+
+    def add_cloze(self, data: str) -> None:
+        model = mw.col.models.by_name("_Cloze")
+        new_note = mw.col.new_note(model)
+        new_note["Text"] = data
+        deck = mw.col.decks.id_for_name("Test")
+        mw.col.add_note(new_note, deck)
